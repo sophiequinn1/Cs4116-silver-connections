d26b0a9926a342cacce7feca4418391d0144d682
sophiequinn1 <21318174@studentmail.ul.ie>
2024-05-03 22:02:56 +0100

    Fix db issues for admin

--- checkAdmin.php
+++ checkAdmin.php
@@ -2,14 +2,14 @@
 require 'config.php';
 
 $userId = $_SESSION['UserId'];
-$sql="SELECT IsAdmin FROM users WHERE UserId = ?";
+$sql="SELECT Admin FROM users WHERE id = ?";
 $stmt = $db->prepare($sql);
 $stmt->bind_param("i", $userId);
 $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $isAdmin = $row['IsAdmin'];
+    $isAdmin = $row['Admin'];
 } else {
     echo "No user found with the provided ID.";
 }
